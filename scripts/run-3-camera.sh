ba7dcca50208f27a0229c48872b790c98b38b0a7
robo <robo1@ryev.net>
2024-11-20 13:51:30 -0800

    Fix paths and make scripts executable

--- scripts/run-3-camera.sh
+++ scripts/run-3-camera.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/depthai-movenet
+cd $SCRIPT_DIR/../depthai-movenet
 
 python3 demo.py -m thunder -f 10 -e
